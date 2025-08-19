eb554c142344d4a7ae2a32b2c32082c22e20709b
5uw1 <suwijakza@gmail.com>
2025-08-19 08:05:53 +0200

    Liskov Substitution Principle bad practice

---
+++ LiskovSubstitutionPrinciple/Program.cs
@@ -0,0 +1,10 @@
+﻿using LiskovSubstitutionPrinciple;
+
+Rectangle rc = new Rectangle(2,3);
+Console.WriteLine($"{rc} has area {Area(rc)}");
+
+Rectangle sq = new Square();
+sq.Width = 4;
+Console.WriteLine($"{sq} has area {Area(sq)}");
+
+static int Area(Rectangle r) => r.Width * r.Height;
---
+++ LiskovSubstitutionPrinciple/Square.cs
@@ -0,0 +1,14 @@
+﻿namespace LiskovSubstitutionPrinciple;
+
+public class Square : Rectangle
+{
+    public new int Width
+    {
+        set => base.Width = base.Height = value;
+    }
+
+    public new int Height
+    {
+        set => base.Width = base.Height = value;   
+    }
+}
